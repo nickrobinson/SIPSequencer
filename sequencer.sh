4a93688852f9ce3dc83926fc9139ed70943663b8
Nick Robinson <user@example.com>
2014-11-20 16:11:17 -0600

    Fixing issue with call to mscgen

--- sequencer.sh
+++ sequencer.sh
@@ -7,4 +7,5 @@ fi
 
 awk -f src/SIPHawk.awk -v callID=$1 $2 > /tmp/testFile
 awk -f src/tester.awk -v ipAddrs=$(awk -f src/SIPSequencer.awk /tmp/testFile) /tmp/testFile > /tmp/mscText
+./dep/mscgen-0.20/bin/mscgen -i /tmp/mscText -T png -o out.png
 
